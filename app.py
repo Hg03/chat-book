a7c02312d151483e7547e59b1b2acf75b04b2c8e
Harish Gehlot <69637720+Hg03@users.noreply.github.com>
2023-07-18 19:27:34 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader, PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma, Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+import pinecone
+import os
+from markdownlit import mdlit
+
+st.set_page_config(layout='wide',page_icon='📟',page_title='Chatbook')
+mdlit('Chat - [blue]book[/blue] ✉️ 🔥')
+
